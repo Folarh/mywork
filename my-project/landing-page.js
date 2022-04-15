2aa9e56cb24f4b6e4f4a48fe151933b3c6579b68
Folarh <98031422+Folarh@users.noreply.github.com>
2022-04-15 13:55:20 -0700

    Add files via upload

---
+++ my-project/landing-page.js
@@ -0,0 +1,18 @@
+document.querySelector(".btn_signup").addEventListener
+("click", function(e){
+console.log("fill in your details to sign up")
+console.log(e)
+});
+
+document.querySelector(".btn_login").addEventListener
+("click", function(e){
+console.log("type in your details to be signed in")
+console.log(e)
+});
+
+document.querySelector(".btn-main").addEventListener
+("click", function(e){
+console.log("getting started")
+console.log(e)
+});
+
